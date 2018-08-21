cbf3b7eeef9d6849d674e64c0d8ffa63c5a5948a
Rachel Grier <rgrier104@gmail.com>
2018-08-21 13:05:44 +0000

    automatically backed up by learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -1,1 +1,11 @@
 # Write your code here.
+def badge_maker(name)
+  return "Hello, my name is #{name}."
+end
+
+def batch_badge_creator(name_array)
+  badge_array = []
+  name_array.each do |name|
+    badge_array << "Hello, my name is #{name}"
+  end
+end
