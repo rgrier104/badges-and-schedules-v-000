ae54007c9df579266a4c8ab9685a03b75ecb911e
Rachel Grier <rgrier104@gmail.com>
2018-08-21 13:23:01 +0000

    automatically backed up by learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -13,6 +13,6 @@ end
 
 def assign_rooms(name_array)
   name_array.each_with_index { |name, index|
-  puts "Hello, #{name}! You will be assigned to room #{index}!"  
+  puts "Hello, #{name}! You will be assigned to room #{index}!"
   }
 end
