239d7b09e7de66ff565f8f4d72dbde1977c9fcc3
Rachel Grier <rgrier104@gmail.com>
2018-08-21 13:29:54 +0000

    automatically backed up by learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -25,7 +25,7 @@ def printer(name_array)
   badge_array.each do |name|
     puts name
   end
-  assign_rooms(name_array).each do |name|
-    puts room_array
+  room_array.each do |name|
+    puts name
   end
 end
